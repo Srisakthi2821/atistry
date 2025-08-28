d4f53d23d020fc0d9abd2aac880e37113477c380
srisakthi <Surya>
2025-08-28 19:08:23 +0530

    3rd commit

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import {Link} from 'react-router-dom'
 import logo_transparent from "../../assets/logo_transparent.png";
 import {
   FaInstagram,
@@ -15,40 +16,40 @@ const Footer = () => (
   <div className="footer">
     <div className="section1">
       <div>
-        <img src={logo_transparent} alt="logo" className="logo_footer" />
-        <h1 className="footer_sec_1_heading">Robby Artistry</h1>
+        <Link to='/' className="link"><img src={logo_transparent} alt="logo" className="logo_footer" /></Link>
+        <Link to='/' className="link"><h1 className="footer_sec_1_heading">Robby Artistry</h1></Link>
       </div>
       <p className="desc_footer">
         Exclusive offers and festive updates await you — stay tuned by following
         our socials
       </p>
       <div className="contanier_social_links">
-        <div className="insta_footer_container">
+        <a href="https://www.instagram.com/robby_artistry/" target="_blank" className="link"><div className="insta_footer_container">
           <FaInstagram className="icon_footer_links insta_footerIcon" />
-        </div>
-        <div  className="whatsapp_footer_container">
+        </div></a>
+        <a href="https://wa.me/7200127265" target="_blank" className="link"><div  className="whatsapp_footer_container">
           <FaWhatsapp className="icon_footer_links" />
-        </div>
-        <div  className="youtube_footer_container">
+        </div></a>
+        <a href="https://www.youtube.com/@robbyartistry" target="_blank" className="link"><div  className="youtube_footer_container">
           <FaYoutube className="icon_footer_links" />
-        </div>
-        <div  className="facebook_footer_container">
+        </div></a>
+        <a href="https://www.facebook.com/share/19ZQgAXNKf/" target="_blank" className="link"><div  className="facebook_footer_container">
           <FaFacebook className="icon_footer_links" />
-        </div>
+        </div></a>
       </div>
     </div>
     <div className="section2">
       <h1 className="footer_sec2_heading">Services</h1>
-      <p className="link_sec2">Blood Art</p>
-      <p className="link_sec2">Photo Frames</p>
-      <p className="link_sec2">Digital Artworks</p>
-      <p className="link_sec2">Imaginary Artworks</p>
+      <Link to='/arts' className="link"><p className="link_sec2">Blood Art</p></Link>
+      <Link to='/arts' className="link"><p className="link_sec2">Photo Frames</p></Link>
+      <Link to='/arts' className="link"><p className="link_sec2">Digital Artworks</p></Link>
+      <Link to='/arts' className="link"><p className="link_sec2">Imaginary Artworks</p></Link>
     </div>
     <div className="section3">
       <h1 className="footer_sec2_heading">Contact</h1>
-      <p className="link_sec2"><span><FaPhoneAlt/></span>+91 72001 27265</p>
-      <p className="link_sec2"><span><FaPhoneAlt/></span>+91 94443 12265</p>
-      <p className="link_sec2"><span><FaInstagramSquare/></span>robby_artistry</p>
+      <a href="tel:+917200127265" className="link"><p className="link_sec2"><FaPhoneAlt/>+91 72001 27265</p></a>
+      <a href="tel:+917200127265" className="link"><p className="link_sec2"><span><FaPhoneAlt/></span>+91 94443 12265</p></a>
+      <a href="https://www.instagram.com/robby_artistry/" target="_blank" className="link"><p className="link_sec2"><span><FaInstagramSquare/></span>robby_artistry</p></a>
       <p className="link_sec2"><span><FaLocationDot/></span>Kanchipuram, TN.</p>
     </div>
   </div>
